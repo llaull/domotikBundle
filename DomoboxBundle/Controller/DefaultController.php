9f6aa37f759bbc15ad9f99cfa4de4c876016a75f
laul <laurent@llovem.eu>
2016-03-20 15:56:46 +0100

    RGB controleur ok en local

--- DomoboxBundle/Controller/DefaultController.php
+++ DomoboxBundle/Controller/DefaultController.php
@@ -4,10 +4,29 @@ namespace Domotique\DomoboxBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
 
     }
+
+
+    public function getModuleColorAction(Request $request)
+    {
+        $curling = $this->container->get('commun.curl');
+
+        $module_url = "http://10.0.112.3/rgb/FFFFFF";
+
+
+        $curl = $curling->getToUrl($module_url, false);
+        return new JsonResponse(array('curl' => $curl));
+
+
+    }
+
+
 }
